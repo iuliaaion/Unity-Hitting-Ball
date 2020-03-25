00179b1d3535536f36711418b5808529a87f30d8
Iulia Ion <50475871+iuliaaion@users.noreply.github.com>
2020-03-25 14:57:12 +0200

    Add files via upload

---
+++ Assets/BallController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // x = move horizontal
+        // y = move up
+        // z = move vertical 
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Ball;
+    private Vector3 offset;
+
+    
+    void Start()
+    {
+        offset = transform.position - Ball.transform.position;
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = Ball.transform.position + offset;
+    }
+}
